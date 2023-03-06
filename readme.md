65c09f16bfbafc392ff9dd679b1ef7620a6f1582
Germain Rullier <grullier@free.fr>
2023-03-06 18:06:42 +0000

    read file

--- readme.md
+++ readme.md
@@ -13,4 +13,6 @@ wget https://chenhaot.com/data/cmv/cmv.tar.bz2
 Extract the dataset:
 ```shell
 tar -xf cmv.tar.bz2
+lbzip2 -d all/train_period_data.jsonlist.bz2
+lbzip2 -d all/heldout_period_data.jsonlist.bz2
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::env;
+use std::fs;
+use serde::Deserializer;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let file_path = &args[1];
+
+    let file_content = fs::read_to_string(file_path).expect("Should have been able to read the file");
+    // Deserializer::deserialize_string(self, visitor);
+
+    println!("{}", file_content);
 }

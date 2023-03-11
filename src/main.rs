a0de1705437f86f4e8c5514089f03d767d1a1fc9
Germain Rullier <grullier@free.fr>
2023-03-11 11:22:59 +0000

    Parse basic

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,39 @@
-use std::env;
-use std::fs;
-use serde::Deserializer;
+use std::{env, io::BufRead};
+use std::fs::File;
+use serde_json::{Value};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let file_path = &args[1];
+    if args.len() != 2 {
+        panic!("Invalid number of arguments.")
+    }
 
-    let file_content = fs::read_to_string(file_path).expect("Should have been able to read the file");
-    // Deserializer::deserialize_string(self, visitor);
+    let file = File::open(&args[1]).unwrap();
 
-    println!("{}", file_content);
+    let mut json_strings = Vec::<String>::new();
+
+    let lines = std::io::BufReader::new(file).lines();
+
+    for line in lines {
+        if let Ok(json_string) = line {
+            json_strings.push(json_string)
+        }
+    }
+
+    let mut jsons = Vec::<Value>::new();
+
+    for json_string in json_strings {
+        let result = serde_json::from_str(&json_string);
+        match result {
+            Ok(value) => {
+                jsons.push(value)    
+            }
+            Err(err) => {
+                panic!("{}", err)
+            }
+        }
+    }
+
+    println!("{}", jsons[0]["domain"]);
 }
